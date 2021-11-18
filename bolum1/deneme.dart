f742ebc1321124d4a415285f0c71a66cbf88d8d2
Baran Zengeralp <73583683+baran2101@users.noreply.github.com>
2021-11-18 21:28:17 +0300

    Delete bolum1 directory

--- bolum1/deneme.dart
+++
@@ -1,6 +0,0 @@
-void main(List<String> args) {
-  int yas = 57;
-  String isim = "John";
-
-  print("İsim : $isim \n Yaş : $yas");
-}
--- bolum1/num_boolean.dart
+++
@@ -1,4 +0,0 @@
-void main(List<String> args) {
-  print("selam");
-  int s1 = 5.6.toInt();
-}
